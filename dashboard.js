a236bad8f78b12ec944b5c3253648a6c1586cb1a
Robert Monfera <monfera.robert@gmail.com>
2015-10-25 01:39:43 +0200

    quickcommit

--- dashboard.js
+++
@@ -1,6 +0,0 @@
-
-function stopScrolling(touchEvent) {touchEvent.preventDefault();}
-document.addEventListener( 'touchstart' , stopScrolling , false )
-document.addEventListener( 'touchmove' , stopScrolling , false )
-
-window.setTimeout(render, 0)
