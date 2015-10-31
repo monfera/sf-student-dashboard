41426f2364b94ad63b7cd2a0e76832fef25081b5
Robert Monfera <monfera.robert@gmail.com>
2015-10-31 13:20:35 +0100

    tweaking the sample tser

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 function sample() {
 
-    // Sampling from a cubic normal distribution to easily generate outliers
+    // Sampling from a  normal distribution raised to a power for frequent generation of outliers
     var tserLength = 8
     var range = Array.apply(Array, Array(tserLength))
     var tsers = ['Insulin-like growth factor', 'Von Willebrand Factor', 'Voltage-gated 6T & 1P', 'Mechanosensitive ion ch.'].map(function (d) {
         return {
             key: d,
             value: range.map(function () {
-                return Math.pow((Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random() - 3) / 3, 3)
+                return (Math.random() > 0.5 ? 1 : -1) * Math.pow(Math.abs(Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random() - 3) / 3, 1.3)
             })
         }
     })
